05ca990037cdd52e7fcb0da219218eb788b420e6
Manchikatlachethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-03-29 19:44:47 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+
+class GFG {
+	public static void main (String[] args) {
+	    //System.out.print("AJSBDSHCKJD");
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        int x=i;
+	        for(int j=1;j<=x;j++)
+	        {
+	            System.out.print("#"+" ");
+	        }
+	        System.out.println();
+	    }
+	    
+		
+	}
+}
