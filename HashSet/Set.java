c0e67983d68c4009993e6302a60d12ce63029116
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-04-28 20:50:03 +0530

    Update Set.java

--- HashSet/Set.java
+++ HashSet/Set.java
@@ -2,7 +2,7 @@
 
 import java.io.*;
 import java.util.*;
-class SetExample {
+class Set {
 	public static void main (String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
