ebf443311b5b23ade859e244fad3730e15c7b36d
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-05-10 17:30:15 +0530

    Create Test.java

---
+++ oops/Test.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Vehicle car=new Vehicle(4,4,"blue","lambergini");
+		car.display(); 
+		car.drive();
+
+	}
+
+}
+
+
+/*sample output
+vehicle color: blue
+ vehicle model: lambergini
+no of wheels for vehicle: 4
+no of passengers in vehicle: 4
