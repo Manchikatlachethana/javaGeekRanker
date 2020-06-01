2983742e94e09549714cd3a5cd4ff7dc6ef78c76
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-01 20:55:51 +0530

    Update PrintTable.java

--- Mathematics/PrintTable.java
+++ Mathematics/PrintTable.java
@@ -1,3 +1,11 @@
+/* 
+prob:Print the table of a given number N. 
+Input:
+First line contains an integer, the number of test cases 'T'. 
+Each testcase cotains one line of input denoting N.
+Output:
+For each testcase, print the table of N upto 10.
+*/
 import java.lang.*;
 import java.io.*;
 class PrintTable
@@ -17,3 +25,10 @@ class PrintTable
 	 }
 	 }
 }
+
+/* sample Input:
+1
+9 
+sample ouput:
+9 18 27 36 45 54 63 72 81 90
+*/
