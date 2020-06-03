b86f465621df02f6d11231bf39b9588c8a05c301
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-03 15:59:29 +0530

    Update ApSeries.java

--- Mathematics/ApSeries.java
+++ Mathematics/ApSeries.java
@@ -1,3 +1,14 @@
+/*
+prob:Given the first 2 terms A and B of an Arithmetic Series, tell the Nth term of the series. 
+Input:The first line of input contains an integer, the number of test cases T.
+Each testcase in its first line should contain two positive integer A and B(First 2 terms of AP). 
+In the second line of every testcase it contains of an integer N.
+output:
+For each testcase, in a new line, print the Nth term of the Arithmetic Progression.
+hint: use APSeries formula for finding nh term : x=a+(n-1)d
+d=(t2-t1),a=first term.
+*/
+
 import java.lang.*;
 import java.io.*;
 class ApSeries
@@ -18,3 +29,15 @@ class ApSeries
         }
 	 }
 }
+
+/* sample Input:
+2
+2 3
+4
+1 2
+10
+
+sample Output:
+5
+10
+*/
