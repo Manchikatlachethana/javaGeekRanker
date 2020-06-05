ba6a3ad1b9d6587beae4196b27abb8623b1c98ae
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-05 16:02:59 +0530

    Update SearchingNum.java

--- Arrays/SearchingNum.java
+++ Arrays/SearchingNum.java
@@ -1,3 +1,17 @@
+/* 
+prob:Given an array of N elements and a integer K.
+Your task is to return the position of first occurence of K in the given array.
+Note: Position of first element is considered as 1.
+Input:
+First line of input contains T denoting the number of testcases. 
+For each testcase there will be two space separated integer N and K denoting the size of array and the value of K respectively. 
+The next line contains the N space separated integers denoting the elements of array.
+
+Output:
+For each test case, print the index of first occurrence of given number K. 
+Prints -1 if the number is not found in array.
+
+*/
 import java.lang.*;
 import java.io.*;
 class SearchingNum
@@ -37,3 +51,16 @@ class SearchingNum
 	 }
 	 }
 }
+
+/* 
+sample Input :
+2 
+5 16
+9 7 2 16 4
+7 98
+1 22 57 47 34 18 66
+
+sample Output : 
+4
+-1
+*/
