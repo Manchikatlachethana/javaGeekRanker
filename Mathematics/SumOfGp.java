6a46b5c1e3e2c07e5610a5e37a9bff69fcde2e4e
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-09 17:45:46 +0530

    Update SumOfGp.java
    need to improvise code to get upto 6 decimal places(21.000000)
    and also to satisfy other testcases.

--- Mathematics/SumOfGp.java
+++ Mathematics/SumOfGp.java
@@ -1,3 +1,13 @@
+/*
+prob:Write a Program to calculate the sum of n terms of a Geometric Progression when first term is a and common ratio is r.
+
+Input: 
+The first line of the input contains T denoting the number of testcases. 
+The first line of the test case will be the number of terms of GP and second line contains first term and common ratio.
+
+Output: 
+For each test case the output will be the sum of n terms of GP.Print the answer upto 6 decimal places.
+*/
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -17,3 +27,13 @@ class SumOfGp
 	 }
 	 }
 }
+
+/*
+Input:
+1
+3
+3 2
+
+Output:
+21.0
+*/
