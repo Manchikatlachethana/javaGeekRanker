a8a3ebd072f80404010d0c5e09a7cf99d8151bff
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-10 20:10:19 +0530

    Create GcdOfTwo.java

---
+++ Mathematics/GcdOfTwo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GcdOfTwo
+ {
+	public static void main (String[] args)
+	 {
+	 Scanner sc=new Scanner(System.in);
+	 int t=sc.nextInt();
+	 for(int i=0;i<t;i++){
+	     int a=sc.nextInt();
+	     int b=sc.nextInt();
+	     System.out.println(gcd(a,b));
+	 }
+	 }
+	 public static int gcd(int a,int b){
+	     if(b==0)
+	     {
+	         return a;
+	     }
+	   return gcd(b,a%b);
+	 }
+}
