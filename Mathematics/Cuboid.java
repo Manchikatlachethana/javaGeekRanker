3ac7287a87a1a372c2785492922bf54f49bd7466
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-12 17:29:29 +0530

    Update Cuboid.java
    can try using other datatypes like double,float..

--- Mathematics/Cuboid.java
+++ Mathematics/Cuboid.java
@@ -1,3 +1,15 @@
+/*
+prob:Given length l, width b and height h of a cuboid. 
+Find the total surface area and volume of cuboid.
+
+Input:
+The first line of the input contains T, denoting the number of testcases. 
+Each of the next T lines contains three space separated positive integers l, b, h
+denoting the length, width and height of cuboid in same line separated by space.
+
+Output:
+For each testcase in new line, output two space separated integers denoting surface area and volume of cuboid.
+*/
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -22,3 +34,12 @@ class Cuboid
 	 
 	 }
 }
+
+/*
+Input:
+1
+1 2 3
+
+Output:
+22 6
+*/
