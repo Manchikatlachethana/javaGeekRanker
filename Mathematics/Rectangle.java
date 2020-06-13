9da3d2a1675965c38e441e258ad9c72705d98da3
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2020-06-13 19:13:20 +0530

    Create Rectangle.java

---
+++ Mathematics/Rectangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Rectangle
+ {
+	public static void main (String[] args)
+	 {
+	 Scanner sc=new Scanner(System.in);
+	 int t=sc.nextInt();
+	 for(int i=0;i<t;i++){
+	     int l=sc.nextInt();
+	     int b=sc.nextInt();
+	     
+	     System.out.println(l*b);
+	 }
+	 }
+}
