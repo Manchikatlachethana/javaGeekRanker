8ab858b2a7a1a52491b693da05989a6f5d209412
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-01-22 12:49:16 +0530

    Update Bsort.java

--- sorting/Bsort.java
+++ sorting/Bsort.java
@@ -5,16 +5,44 @@ class Bsort {
 		int a[] = {3,2,1};
 		int n = a.length;
 		for(int i=0;i<n;i++){
-		    for(int j=0;j<n-i-1;j++){
-		        if(a[j]>a[j+1]){
-		            int temp = a[j];
-		            a[j] = a[j+1];
-		            a[j+1] = temp;
+		    for(int j=i+1;j<n;j++){
+		        if(a[i]>a[j]){
+		            int temp = a[i];
+		            a[i] = a[j];
+		            a[j] = temp;
 		        }
 		    }
 		}
 		for(int i=0;i<n;i++){
 		    System.out.print(a[i]+" ");
 		}
+
 	}
 }
+
+
+
+
+
+
+
+// import java.io.*;
+
+// class Bsort {
+//     public static void main (String[] args) {
+// 		int a[] = {3,2,1};
+// 		int n = a.length;
+// 		for(int i=0;i<n;i++){
+// 		    for(int j=0;j<n-i-1;j++){
+// 		        if(a[j]>a[j+1]){
+// 		            int temp = a[j];
+// 		            a[j] = a[j+1];
+// 		            a[j+1] = temp;
+// 		        }
+// 		    }
+// 		}
+// 		for(int i=0;i<n;i++){
+// 		    System.out.print(a[i]+" ");
+// 		}
+// 	}
+// }
