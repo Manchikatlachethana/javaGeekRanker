9ff742054366e26c52bfd3918171e900b595b35f
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-04-12 16:28:44 +0530

    Day1 : 2

--- PrepKit/MinMax.java
+++ PrepKit/MinMax.java
@@ -1,19 +1,13 @@
 
 public static void miniMaxSum(List<Integer> arr) {
     // Write your code here
-    int max = arr.get(0);
-    int min = arr.get(0);
-    int sum = 0;
+    long max = arr.get(0);
+    long min = arr.get(0);
+    long sum = 0;
     for(int i:arr){
         sum = sum+i;
-        if(i<min){
-            min = i;
-        }
-        if(i>max){
-            max = i;
-        }
-    }
-    
+        max = Math.max(max,i);
+        min = Math.min(min,i);
+    }   
     System.out.println((sum-max)+" "+(sum-min));
-
 }
