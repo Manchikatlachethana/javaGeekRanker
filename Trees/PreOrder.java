8c4ffd4ade5c6389c5addddd7c3688030bed2099
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-04-25 16:44:21 +0530

    Create PreOrder.java

---
+++ Trees/PreOrder.java
@@ -0,0 +1,35 @@
+public static void preOrder(Node root) {
+        if(root == null){
+            return;
+        }
+        Stack<Node> s = new Stack();
+        List<Integer> l = new ArrayList<>();
+        s.push(root);
+        while(!s.isEmpty()){
+            Node curr = s.pop();
+            l.add(curr.data);
+            if(curr.left != null){
+                s.push(curr.left);
+            }
+            if(curr.right != null){
+                s.push(curr.right);
+            }
+            
+        }
+        Collections.reverse(l);
+        for(int i:l){
+            System.out.print(i+" ");
+        }      
+    }
+
+
+/* Recursion
+public static void preOrder(Node root) {
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+*/
