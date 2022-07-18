f98e6664fe9ffb50909265a3dc4d8bbed79b3fc8
chethana <62558574+Manchikatlachethana@users.noreply.github.com>
2022-07-18 15:01:41 +0530

    Recursion and Iterative

--- Trees/InOrder.java
+++ Trees/InOrder.java
@@ -5,5 +5,26 @@ public static void inOrder(Node root) {
         inOrder(root.left);
         System.out.print(root.data+" ");
         inOrder(root.right);
-
     }
+
+/* iterative
+
+public static void inOrder(Node root) {
+        if(root == null) return;
+        Stack<Node> s = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        Node curr = root;
+        while(!s.isEmpty() || curr != null){
+            while(curr != null){
+                s.push(curr);
+                curr = curr.left;
+            }
+            curr = s.pop();
+            result.add(curr.data);
+            curr = curr.right;
+        }
+        for(int i:result){
+            System.out.print(i+" ");
+        }
+    }
+ */
